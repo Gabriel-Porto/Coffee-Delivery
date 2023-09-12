6eeb27b7fedb21be586a23fa3a9b54b11908f5e8
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-09-12 12:12:38 -0300

    feat: Home page hero

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import { HomeContainer, Hero } from './styles'
+import heroImg from '../../assets/hero-img.svg'
+import { ShoppingCart, Timer, Coffee, Package } from '@phosphor-icons/react'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <Hero>
+        <div className="heroText">
+          <div className="heroTitle">
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </div>
+          <div className="heroItems">
+            <span>
+              <ShoppingCart weight="fill" />
+              <p>Compra simples e segura</p>
+            </span>
+            <span>
+              <Package weight="fill" />
+              <p>Embalagem mantém o café intacto</p>
+            </span>
+            <span>
+              <Timer weight="fill" />
+              <p>Entrega rápida e rastreada</p>
+            </span>
+            <span>
+              <Coffee weight="fill" />
+              <p>O café chega fresquinho até você</p>
+            </span>
+          </div>
+        </div>
+
+        <img src={heroImg} alt="" />
+      </Hero>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+import heroBackground from '../../assets/hero-background.svg'
+
+export const HomeContainer = styled.main`
+  display: flex;
+`
+
+export const Hero = styled.section`
+  display: flex;
+  gap: 3.5rem;
+  height: 34rem;
+
+  padding: 5.75rem 10rem;
+
+  background-image: url(${heroBackground});
+
+  .heroText {
+    display: flex;
+    gap: 4.12rem;
+    flex-direction: column;
+
+    .heroTitle {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      h1 {
+        color: ${({ theme }) => theme.colors['base-title']};
+        font-family: 'Baloo 2', sans-serif;
+        font-size: 3rem;
+        font-weight: 800;
+      }
+
+      p {
+        color: ${({ theme }) => theme.colors['base-subtitle']};
+        font-size: 1.25rem;
+      }
+    }
+
+    .heroItems {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 1.5rem 2.5rem;
+
+      span {
+        width: max-content;
+
+        display: flex;
+        align-items: center;
+        gap: 0.75rem;
+
+        svg {
+          width: 2rem;
+          height: 2rem;
+
+          padding: 0.5rem;
+          color: ${({ theme }) => theme.colors.white};
+
+          border-radius: 62.5rem;
+        }
+
+        &:nth-child(1) svg {
+          background-color: ${({ theme }) => theme.colors['yellow-dark']};
+        }
+        &:nth-child(2) svg {
+          background-color: ${({ theme }) => theme.colors['base-text']};
+        }
+        &:nth-child(3) svg {
+          background-color: ${({ theme }) => theme.colors.yellow};
+        }
+        &:nth-child(4) svg {
+          background-color: ${({ theme }) => theme.colors.purple};
+        }
+      }
+    }
+  }
+
+  img {
+    width: 29.75rem;
+    height: 22.5rem;
+  }
+`
